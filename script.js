2da57f97bae67779c3f53335c1b0a1cf2c216dbb
X-DAY007 <42087296+X-DAY007@users.noreply.github.com>
2025-01-05 09:02:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Button 1: Redirect to index1.html
+document.getElementById('button1').addEventListener('click', () => {
+  window.location.href = '/storage/emulated/0/AAAA-1/X-Seed-Checker/Seed-CheckerBIP39/index.html';
+});
+
+// Button 2: Redirect to index2.html
+document.getElementById('button2').addEventListener('click', () => {
+  window.location.href = '/storage/emulated/0/AAAA-1/X-Seed-Checker/Seed-CheckerFileSeed/index.html';
+});
+
+// Contact Link: Open Telegram profile
+document.getElementById('contact').addEventListener('click', () => {
+  window.open('https://t.me/xss_id', '_blank');
+});
